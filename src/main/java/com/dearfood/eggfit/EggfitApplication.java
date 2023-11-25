e7dd55cbc168b1d9e1e082f0ac4e9b6cbbb38fdf
seohee-P <p03061180@gmail.com>
2023-11-25 16:10:23 +0900

    Update Settings

---
+++ src/main/java/com/dearfood/eggfit/EggfitApplication.java
@@ -0,0 +1,13 @@
+package com.dearfood.eggfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggfitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dearfood/eggfit/domain/User.java
@@ -0,0 +1,4 @@
+package com.dearfood.eggfit.domain;
+
+public class User {
+}
